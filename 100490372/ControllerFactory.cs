c9ebba116ac646392d54fdb3b421db63d11dad8a
Brettskibols <brettski_darkside@hotmail.co.uk>
2019-04-19 16:14:30 +0100

    Porthos and Boogle intro work

---
+++ 100490372/ControllerFactory.cs
@@ -0,0 +1,25 @@
+﻿using CritterController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100490372
+{
+    class ControllerFactory : ICritterControllerFactory
+    {
+        public string Author => "100490372";
+
+        public ICritterController[] GetCritterControllers()
+        {
+            List<ICritterController> controllers = new List<ICritterController>();
+            for (int i = 0; i < 25; i++)
+            {
+                controllers.Add(new Porthos("Porthos" + (i + 1)));
+                controllers.Add(new Boogle("Boogle" + (i + 1)));
+            }
+            return controllers.ToArray();
+        }
+    }
+}
